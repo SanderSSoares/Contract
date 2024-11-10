7fea7439b49683297ab6526a0416135b645e57de
SanderSSoares <sanderl.santos@gmail.com>
2024-11-10 10:51:49 +0000

    Creating menu and items available

--- contracts/RestaurantContract.sol
+++ contracts/RestaurantContract.sol
@@ -1,3 +1,18 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.7;
+
+//Define the restaurant contract
+contract Restaurant {
+
+    address public owner;
+    uint public ownerBalance;
+
+    struct MenuItem {
+
+        string name; //Name of the Item (Burgers, Ceg)
+        uint price;  //Price in Wel (coin)
+
+
+    }
+}
